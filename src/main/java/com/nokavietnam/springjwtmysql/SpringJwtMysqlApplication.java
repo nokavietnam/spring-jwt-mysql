58fbb483b2e58cecc96e8103cdda78422dc862e7
noka <noka.hieunt@gmail.com>
2022-01-02 16:28:00 +0700

    update

---
+++ src/main/java/com/nokavietnam/springjwtmysql/SpringJwtMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.nokavietnam.springjwtmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtMysqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJwtMysqlApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nokavietnam/springjwtmysql/SpringJwtMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokavietnam.springjwtmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtMysqlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
